1599eed499be0591ac76d356b4e482dd4046e296
Abdiox <124003288+Abdiox@users.noreply.github.com>
2026-05-28 20:05:06 +0200

    version control i controlleren

--- src/main/java/com/dls/courierservice/Controller/DeliveryController.java
+++ src/main/java/com/dls/courierservice/Controller/DeliveryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/deliveries")
+@RequestMapping("/api/v1/deliveries")
 public class DeliveryController {
 
     private final DeliveryService deliveryService;
